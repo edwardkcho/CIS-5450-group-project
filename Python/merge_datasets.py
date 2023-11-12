4b9b5bbecebacc5f96cbd380cc2066b66e79830d
Edward Cho <80547448+edwardkcho@users.noreply.github.com>
2023-11-12 01:45:58 -0500

    remove unused library

--- Python/merge_datasets.py
+++ Python/merge_datasets.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import datetime as dt
 
 # Load the first spreadsheet
 article_titles_with_stocks = pd.read_csv('../RawDataSets/analyst_ratings_processed.csv', skiprows=[1])
@@ -26,27 +25,19 @@ stock_price_lookup = stock_price_lookup[[x for x in stock_price_lookup.columns i
 
 stock_price_lookup = stock_price_lookup.dropna(axis=1, how='all')
 
-# print(stock_price_lookup.head(5))
-# print(str(stock_price_lookup.loc['2010-01-04', 'A']))
-
 # Add new column to get stock price info in articles df
 article_titles_with_stocks['closing-price'] = np.NAN
 
 for index, row in article_titles_with_stocks.iterrows():
-    # row['closing-price'] = stock_price_lookup[row.at[1, 'date'], row.at[1, 'stock']]
     date_lookup = str(article_titles_with_stocks.at[index, 'date']).split(' ')[0]
     stock_lookup = article_titles_with_stocks.at[index, 'stock']
     test = str(stock_price_lookup.loc['2020-06-03', 'A'])
     try:
         article_titles_with_stocks.at[index, 'closing-price'] = \
-            stock_price_lookup.loc[date_lookup, stock_lookup]
+            stock_price_lookup.loc[date_lookup, stock_lookup].round(2)
     except KeyError:
         article_titles_with_stocks.at[index, 'closing-price'] = np.nan
 
-# Merge the dataframes based on 'stock' and 'date'
-article_titles_with_stocks.dropna()
-# Display the resulting DataFrame
-print(article_titles_with_stocks.head(5))
+article_titles_with_stocks = article_titles_with_stocks.dropna()
 print("Writing to csv file in CleanedDataSets directory")
-
 article_titles_with_stocks.to_csv('../CleanedDataSets/cleaned_dataset.csv', header=True, index=False)
